9e9339f697c7e07ad9e8da794ac84c1344390012
karktya <96881324+karktya@users.noreply.github.com>
2022-01-05 17:36:21 +0200

    Add files via upload

---
+++ map_sprites1/Dungeon_Tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="Dungeon_Tileset" tilewidth="32" tileheight="32" margin="1" tilecount="285" columns="15">
+ <image source="Dungeon_Tileset.png" width="512" height="640"/>
+</tileset>
